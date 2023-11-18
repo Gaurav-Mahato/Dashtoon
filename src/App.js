290684cb6ba6fcedd55233bdcab2982e76a9ded5
GauravMahato <Gauravmahato1309@gmail.com>
2023-11-18 21:38:49 +0530

    Added publish Screen

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import Header from './components/Header';
 import MangaScreen from './components/MangaScreen';
 import './components/bootstrap.min.css'
+import PublishScreen from './components/PublishScreen';
+import {useSelector} from "react-redux"
 
 function App() {
   // const formHandler = (e) => {
   //   e.preventDefault()
     
   // }
-  return (<>
+  const {panel} = useSelector(state => state.panel)
+  return <>
         <Header />
-        <MangaScreen />
-    </>);
+        {panel.length < 10 ? <MangaScreen /> : <PublishScreen />}
+        {/* <PublishScreen /> */}
+    </>
 }
 
 export default App;
